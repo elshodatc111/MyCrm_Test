44039332836a3ba45003d681ab04aa44081e2ddc
elshodatc111 <elshodatc1116@gmail.com>
2024-05-09 13:47:23 +0300

    Save

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -7,13 +7,13 @@
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://crm-atko.uz/online/css/animate.css">
-		<link rel="stylesheet" href="https://crm-atko.uz/online/css/owl.carousel.min.css">
-		<link rel="stylesheet" href="https://crm-atko.uz/online/css/owl.theme.default.min.css">
-		<link rel="stylesheet" href="https://crm-atko.uz/online/css/magnific-popup.css">
-		<link rel="stylesheet" href="https://crm-atko.uz/online/css/flaticon.css">
+		<link rel="stylesheet" href="{{ env('HTTP_URL')}}/css/animate.css">
+		<link rel="stylesheet" href="{{ env('HTTP_URL')}}/css/owl.carousel.min.css">
+		<link rel="stylesheet" href="{{ env('HTTP_URL')}}/css/owl.theme.default.min.css">
+		<link rel="stylesheet" href="{{ env('HTTP_URL')}}/css/magnific-popup.css">
+		<link rel="stylesheet" href="{{ env('HTTP_URL')}}/css/flaticon.css">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://crm-atko.uz/online/css/style.css">
+		<link rel="stylesheet" href="{{ env('HTTP_URL')}}/css/style.css">
 		
 	</head>
 <body>
@@ -89,20 +89,20 @@
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  <script src="https://crm-atko.uz/online/js/jquery.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/popper.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/bootstrap.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/jquery.easing.1.3.js"></script>
-  <script src="https://crm-atko.uz/online/js/jquery.waypoints.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/jquery.stellar.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/owl.carousel.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/jquery.magnific-popup.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/jquery.animateNumber.min.js"></script>
-  <script src="https://crm-atko.uz/online/js/scrollax.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/jquery.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/popper.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/bootstrap.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/jquery.easing.1.3.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/jquery.waypoints.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/jquery.stellar.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/owl.carousel.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/jquery.magnific-popup.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/jquery.animateNumber.min.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/scrollax.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="https://crm-atko.uz/online/js/google-map.js"></script>
-  <script src="https://crm-atko.uz/online/js/main.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/google-map.js"></script>
+  <script src="{{ env('HTTP_URL')}}/js/main.js"></script>
     
   </body>
 </html>
